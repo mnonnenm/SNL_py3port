c2af2df1490f0abcc994dd52d88e88597032a97e
mnonnenm <mnonnenm@uos.de>
2018-11-27 12:01:57 +0100

    licence fix in setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version='0.1',
     description='Sequential neural likelihood',
     url='https://github.com/gpapamak/snl',
-    author='mnonnenm',
+    author='Marcel Nonnenmacher (original code from George Papamakarios)',
     packages=['snl', 'snl.ml', 'snl.ml.models', 'snl.pdfs', 'snl.simulators', 'snl.util', 
               'snl.inference', 'snl.inference.diagnostics'],
-    license='BSD',
+    license='MIT',
     install_requires=['dill', 'lasagne==0.2.dev1', 'numpy', 'scipy', 'theano', 'tqdm'],
     dependency_links=[
         'https://github.com/Lasagne/Lasagne/archive/master.zip#egg=lasagne-0.2.dev1',
